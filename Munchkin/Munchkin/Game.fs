2ea834ba241b18a871920762e42e0c603cc962b4
Philip Persson <kalllez123@hotmail.se>
2016-08-16 21:38:23 +0200

    Card is discarded

--- Munchkin/Munchkin/Game.fs
+++ Munchkin/Munchkin/Game.fs
@@ -13,12 +13,13 @@ module Game =
     let startFight (state: GameState) (monster : MonsterCard) =
         if state.CurrentPlayer.Level <= monster.MinimumPlayerLevel then state
         else
-        { state with MonstersFighting =  monster :: state.MonstersFighting; TurnState = TurnState.FightingMonster;  DoorCards = state.DoorCards |> List.except [DoorCard.MonsterCard monster] }
+        { state with MonstersFighting =  monster :: state.MonstersFighting; TurnState = TurnState.FightingMonster;  DoorCards = state.DoorCards |> List.except [DoorCard.MonsterCard monster]; DiscardedDoorCards = DoorCard.MonsterCard monster :: state.DiscardedDoorCards }
         
     let handleCardDrawn card state =
         match card with
         | MonsterCard(m) -> startFight state m
-        | CurseCard(c) -> { state with TurnState = TurnState.LootOrSummonMonster; DoorCards = state.DoorCards |> List.except [DoorCard.CurseCard c] }
+        | CurseCard(c) -> 
+        { state with TurnState = TurnState.LootOrSummonMonster; DoorCards = state.DoorCards |> List.except [DoorCard.CurseCard c]; DiscardedDoorCards = DoorCard.CurseCard c :: state.DiscardedDoorCards }
     
 
     let startTurn (state : GameState) =
