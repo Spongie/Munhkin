3fdab097c2c996cba61ba08bc854441b68fc05dd
Philip Persson <kalllez123@hotmail.se>
2016-08-19 16:18:07 +0200

    Add buffs in combat

--- Munchkin/Munchkin/Battle.fs
+++ Munchkin/Munchkin/Battle.fs
@@ -7,3 +7,8 @@ module Battle =
     let createBattle monster =
         { MonstersFighting = [monster]; PlayerBuffs = []; MonsterBuffs = [] }
 
+    let addBuff buffCard toMonster battle =
+        if toMonster then {battle with MonsterBuffs = buffCard :: battle.MonsterBuffs }
+        else
+        {battle with MonsterBuffs = buffCard :: battle.PlayerBuffs}
+
