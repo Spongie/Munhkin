44b18ab07f427d970e7753ab865b2441333441c8
Philip Persson <kalllez123@hotmail.se>
2016-08-27 15:30:15 +0200

    get total monsters level

--- Munchkin/Munchkin/Battle.fs
+++ Munchkin/Munchkin/Battle.fs
@@ -18,3 +18,15 @@ module Battle =
         |>List.map (fun x -> x.Bonus)
         |>List.sum
         |> (+) (getPlayerLevel  state.CurrentPlayer)
+
+    let getMonstersBaseLevel battle =
+        battle.MonstersFighting
+        |>List.map (fun x -> x.Level)
+        |>List.sum
+
+    let getMonsterTotalLevel battle state =
+        battle.MonsterBuffs
+        |>List.map (fun x -> x.Bonus)
+        |>List.sum
+        |> (+) (getPlayerLevel  state.CurrentPlayer)
+        |> (+) (getMonstersBaseLevel battle)
