50d8c8cb7e71f822c434340e20e81a0e9fffc1e0
Philip Persson <kalllez123@hotmail.se>
2016-08-28 16:22:31 +0200

    tmp

--- Munchkin/Munchkin/Game.fs
+++ Munchkin/Munchkin/Game.fs
@@ -14,13 +14,21 @@ module Game =
     let startFight state monster =
         if state.CurrentPlayer.Level <= monster.MinimumPlayerLevel then state
         else
-        { state with CurrentBattle = createBattle monster; TurnState = TurnState.FightingMonster;  DoorCards = state.DoorCards |> List.except [DoorCard.MonsterCard monster] }
+        { state with 
+            CurrentBattle = createBattle monster
+            TurnState = TurnState.FightingMonster
+            DoorCards = state.DoorCards 
+                        |> List.except [DoorCard.MonsterCard monster] 
+        }
         
     let handleCardDrawn card state =
         match card with
         | MonsterCard(m) -> startFight state m
         | CurseCard(c) -> { state with TurnState = TurnState.LootOrSummonMonster; DoorCards = state.DoorCards |> List.except [DoorCard.CurseCard c]; DiscardedDoorCards = DoorCard.CurseCard c :: state.DiscardedDoorCards }
-        | _ -> {state with TurnState = TurnState.LootOrSummonMonster; CurrentPlayer = {state.CurrentPlayer with Cards = Card.DoorCard card :: state.CurrentPlayer.Cards } }
+        | _ -> { state with 
+                    TurnState = TurnState.LootOrSummonMonster
+                    CurrentPlayer = { state.CurrentPlayer with 
+                                        Cards = Card.DoorCard card :: state.CurrentPlayer.Cards } }
         
     let startTurn state =
         let doorCard = drawRandomDoorCard state
